6d6e11211666b7c88255c2d40fed4c8f0b385599
Jhonny Quiliche <jhonny.jql1210@gmail.com>
2023-01-26 21:50:41 -0500

    First commit :fire:

---
+++ src/interface/Pokemon.ts
@@ -0,0 +1,16 @@
+export interface PokemonType {
+  id: number,
+  name: string,
+  sprites: {
+      back_default: string,
+      front_default: string,      
+  },
+  stats: ItemStat[];
+}
+
+export interface ItemStat{
+  base_stat: string,
+  stat: {
+    name: string,
+  }   
+}
